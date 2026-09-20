5755ccfdf6c0f8f366d2066249f7eeafba3c1220
Julio Bitencourt <jujujulio345soulegal@gmail.com>
2026-09-20 12:19:47 -0300

    feat: Adicionado gráfico de estabelecimentos por estado

--- __main__.py
+++ __main__.py
@@ -1,8 +1,10 @@
 from classes import *
 
+
 def main():
-    obj_1 = Consultas('Leitos_2026.csv')
-    obj_1.Mostrar_Hospitais()
+    obj_1 = Analise('Leitos_2026.csv')
+    obj_2 = Consultas('Leitos_2026.csv')
+    obj_1.estabelecimentos_por_estado()
 
 if __name__ == '__main__':
     main()
--- classes.py
+++ classes.py
@@ -1,17 +1,18 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 
 class Consultas:
-    def __init__(self, URL):
-        self.df = pd.read_csv(URL, sep=';', encoding='latin1')
+    def __init__(self, url):
+        self.df = pd.read_csv(url, sep=';', encoding='latin1')
 
 
-    def Mostrar_Hospitais(self, estado:str = 'sp', limite_hospitais:int = 10):
+    def mostrar_estabelecimentos(self, estado:str = 'sp', limite_estabelecimentos:int = 10):
         '''
         :param estado: Estado que os hospitais residem
-        :param limite_hospitais: quantidade de hospitais que serão mostrados, caso nada seja passado, 10 hospitais serão mostrados
+        :param limite_estabelecimentos: quantidade de hospitais que serão mostrados, caso nada seja passado, 10 hospitais serão mostrados
         '''
         estado = estado.upper()
         temp_df = self.df.copy()
@@ -24,4 +25,20 @@ class Consultas:
             temp_df = temp_df.fillna({'NU_ENDERECO': 'Endereço não informado', 'NU_TELEFONE': 'Telefone não informado', 'NO_EMAIL': 'Email não encontrado'})
 
             #Informações mostradas
-            print(temp_df[['NOME_ESTABELECIMENTO', 'NU_TELEFONE', 'NU_ENDERECO', 'NO_BAIRRO', 'NO_EMAIL']][temp_df['UF'] == estado].head(limite_hospitais))
+            print(temp_df[['NOME_ESTABELECIMENTO', 'NU_TELEFONE', 'NU_ENDERECO', 'NO_BAIRRO', 'NO_EMAIL']][temp_df['UF'] == estado].head(limite_estabelecimentos))
+
+
+class Analise:
+    def __init__(self, url):
+        self.df = pd.read_csv(url, sep=';', encoding='latin1')
+
+    def estabelecimentos_por_estado(self):
+        novo_df = self.df.copy()
+        df_temp = novo_df.groupby(['UF']).nunique()['CNES']
+        estados, quant = [*df_temp.index], [*df_temp.values]
+        plt.barh(estados, quant)
+        plt.xlabel('Quantida de estabelecimentos')
+        plt.ylabel('Estados')
+        plt.title('Estabelecimentos de sáude por estado.')
+        plt.show()
+
